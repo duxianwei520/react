9002e31e0550e28b8c3d66570b6cadf471449605
dupi <duxianwei520@126.com>
2019-05-22 20:21:23 +0800

    dupi 更新antd版本以及测试环境的配置

--- scripts/webpack.testing.config.js
+++ scripts/webpack.testing.config.js
@@ -2,25 +2,85 @@
 const webpack = require('webpack')
 const path = require('path')
 const merge = require('webpack-merge')
-const webpackConfigBase = require('./webpack.prod.config')
+const webpackConfigBase = require('./webpack.base.config')
+const Copy = require('copy-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
+const AutoDllPlugin = require('autodll-webpack-plugin');
 
 function resolve(relatedPath) {
   return path.join(__dirname, relatedPath)
 }
 
 const webpackConfigProd = {
+  output: {
+    publicPath: './',
+  },
   plugins: [
     // 定义环境变量为开发环境
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('testing'),
       IS_DEVELOPMETN: false,
     }),
-    // 将打包后的资源注入到html文件内 
+    // 将打包后的资源注入到html文件内    
     new HtmlWebpackPlugin({
+      // inject: true, // will inject the main bundle to index.html
       template: resolve('../app/index.html'),
-      mapConfig:'http://192.168.0.1/map_config.js'      
+      mapConfig:'http://192.168.0.1/map_config.js'
     }),
+    /* webpack自带压缩代码*/
+    // new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    /* 多核压缩代码 */
+    new ParallelUglifyPlugin({
+      cacheDir: '.cache/',
+      uglifyJS:{
+        output: {
+          comments: false
+        },
+        compress: {
+          warnings: false
+        }
+      }
+    }),
+    // 分析代码
+    new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
+    // new Copy([
+    //   { from: './app/images', to: './images' },
+    // ]),
+    new CleanWebpackPlugin(['dist'],{
+      root: path.join(__dirname, '../'),
+      verbose:false,
+      // exclude:['img']//不删除img静态资源
+    }),
+    new AutoDllPlugin({
+      inject: true, // will inject the DLL bundle to index.html
+      debug: true,
+      filename: '[name].1.0.js',
+      path: './dll',
+      entry: {
+        dll: [
+          'react',
+          'react-dom',
+          'react-router',
+          'babel'
+        ]
+      },
+      plugins: [
+        new ParallelUglifyPlugin({
+          cacheDir: '.cache/',
+          uglifyJS:{
+            output: {
+              comments: false
+            },
+            compress: {
+              warnings: false
+            }
+          }
+        }),
+      ],
+    })
   ],
 }
 
