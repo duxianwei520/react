10d50fb6df7f90b4264c083b5bb5dec216cb9706
dupi <duxianwei520@126.com>
2020-05-17 17:49:55 +0800

    dupi update function to get user ip address

--- scripts/webpack.dev.config.js
+++ scripts/webpack.dev.config.js
@@ -5,15 +5,30 @@ const merge = require('webpack-merge')
 const webpackConfigBase = require('./webpack.base.config')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
-const os = require('os');
-let selfIp;
+const os = require('os')
+let selfIp
 try {
-  selfIp = os.networkInterfaces()['WLAN'][1].address;
+  // selfIp = os.networkInterfaces()['WLAN'][1].address
+  selfIp = getIpAddress()
 } catch (e) {
   selfIp = 'localhost'
 }
 
 const PORT = 8888
+// 精确的获取本机ip地址
+function getIpAddress () {
+  const interfaces = require('os').networkInterfaces
+  for (let devName in interfaces) {
+    const iface = interfaces[devName]
+    for (let i = 0; i < iface.length; i += 1) {
+      let alias = iface[i]
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address
+      }
+    }
+  }
+}
+
 function resolve(relatedPath) {
   return path.join(__dirname, relatedPath)
 }
